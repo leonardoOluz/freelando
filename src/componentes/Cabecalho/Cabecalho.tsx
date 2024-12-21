ba6297b26b07f436fe6687236a666ba99478787b
Leonardo Luz <leonardoluz10@hotmail.com>
2024-12-20 21:43:02 -0300

    Novo componente cabeçalho.

---
+++ src/componentes/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled"
+
+export const Cabecalho = styled.header`
+  background-color: ${({ theme }) => theme.cores.primarias.a};
+  padding: ${({ theme }) => theme.espacamentos.m};
+`;
