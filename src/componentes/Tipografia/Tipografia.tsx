48f06458ffeb675bf1845cb76c4728bf8c19c81b
Leonardo Luz <leonardoluz10@hotmail.com>
2024-12-22 18:31:30 -0300

    Componente Tipografia criado.

---
+++ src/componentes/Tipografia/Tipografia.tsx
@@ -0,0 +1,85 @@
+import styled from "@emotion/styled";
+
+interface IComponentes {
+  h1: string;
+  h2: string;
+  h3: string;
+  body: string;
+}
+
+interface IEstilos {
+  h1: string;
+  h2: string;
+  h3: string;
+  body: string;
+  body2: string;
+  subtititle1: string;
+  subtititle2: string;
+}
+
+interface PropsTipografia {
+  variante: VarianteTipo;
+  componente: ComponenteTipo;
+  children: React.ReactNode
+}
+
+type ComponenteTipo = keyof IComponentes
+
+type VarianteTipo = keyof IEstilos
+
+const componentes: IComponentes = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  body: "p"
+}
+
+const estilos: IEstilos = {
+  h1: `
+    font-size: 4rem;
+    font-weight: 600;
+    line-height: 4.9rem;
+  `,
+  h2: `
+    font-size: 3.2rem;
+    font-weight: 600;
+    line-height: 3.9rem;
+    
+  `,
+  h3: `
+    font-size: 2.4rem;
+    font-weight: 500;
+    line-height: 2.9rem;
+    
+  `,
+  body: `
+    font-size: 2rem;
+    font-weight: 400;
+    line-height: 2.4rem;
+  `,
+  body2: `
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 2rem;
+  `,
+  subtititle1: `
+    font-size: 1.4rem;
+    font-weight: 400;
+    line-height: 1.7rem;
+  `,
+  subtititle2: `
+    font-size: 1.4rem;
+    font-weight: 400;
+    line-height: 1.7rem;
+    text-decoration: underline;
+  `,
+
+}
+
+export const Tipografia = ({ variante, componente, children }: PropsTipografia) => {
+  const tag = componentes[componente] as keyof JSX.IntrinsicElements;
+  const ComponenteUtilizado = styled[tag as "p"]`${estilos[variante]}`;
+  return (<ComponenteUtilizado>
+    {children}
+  </ComponenteUtilizado>)
+}
