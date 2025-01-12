4bb3bdd54df41cccfbe6112d6c46740beda21430
Leonardo Luz <leonardoluz10@hotmail.com>
2025-01-11 21:25:04 -0300

    feat: :white_check_mark: Componente de selecaoCliente, LayoutBase e LayoutBaseCadastro criado e reenderizado no router.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import SelecaoCliente from "../Pages/cadastro/SelecaoCliente";
+import LayoutBaseCadastro from "../Pages/cadastro/LayoutBaseCadastro";
+import LayoutBase from "../Pages/LayoutBase";
 // import PaginaInicial from "../Pages/PaginaInicial";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <LayoutBase />,
     children: [
       {
         path: "cadastro",
-        element: <SelecaoCliente />,
+        element: <LayoutBaseCadastro />,
         children: [
+          {
+            path: "",
+            element: <SelecaoCliente />
+          },
           {
             path: "cliente",
             element: <h1>Crie seu cadastro</h1>
